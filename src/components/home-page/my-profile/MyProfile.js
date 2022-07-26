802745007b476e2c6ff2a38457660168c6c7acf0
ashayc247 <ashay.t@chapter247.com>
2022-07-26 17:52:53 +0530

    UI: changed view for home page

---
+++ src/components/home-page/my-profile/MyProfile.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import localStorageKeys from 'constants/local-storage-keys';
+
+import styles from './MyProfile.module.scss';
+
+function MyProfile(props) {
+
+  const { profile } = props;
+
+  const navigate = useNavigate();
+
+  function handleLogoutControlClick() {
+
+    localStorage.removeItem(localStorageKeys.USER_DATA);
+
+    navigate('/');
+
+  }
+
+  const profileImageAttributes = {
+    src: profile.profileImage,
+    className: styles.profileImage
+  };
+
+  const fullName = `${profile.firstName} ${profile.lastName}`;
+
+  const profileImageAlternativeText = `${fullName}-profile`;
+
+  const logoutControlAttributes = {
+    className: `application-themed-button ${styles.logoutButton}`,
+    onClick: handleLogoutControlClick
+  };
+
+  return (
+    <div id={styles.myProfileMain}>
+
+      <img {...profileImageAttributes} alt={profileImageAlternativeText} />
+      <h2 className={styles.name}>{fullName}</h2>
+      <h5 className={styles.email}>{profile.email}</h5>
+      <label className={styles.id}>{profile.id}</label>
+      <label className={styles.loggedInStatus}>Logged in with {profile.loginWith}</label>
+
+      <button {...logoutControlAttributes}>Logout</button>
+
+    </div>
+  );
+}
+
+export default MyProfile;
--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -1,27 +1,16 @@
 import React from 'react';
 
-import { Navigate, useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+
+import MyProfile from 'components/home-page/my-profile/MyProfile';
 
 import { getUserStorageData } from 'utilities/storage';
 
-import bannerImage from 'assets/images/home-banner-image.png';
-
 import styles from './HomePage.module.scss';
-import localStorageKeys from 'constants/local-storage-keys';
 
 function HomePage() {
 
   const userStorageData = getUserStorageData();
-
-  const navigate = useNavigate();
-
-  function handleLogoutControlClick() {
-
-    localStorage.removeItem(localStorageKeys.USER_DATA);
-
-    navigate('/');
-
-  }
 
   if (userStorageData === null) {
 
@@ -33,21 +22,13 @@ function HomePage() {
     return <Navigate {...navigateProperties} />;
   }
 
-  const bannerImageAttributes = {
-    src: bannerImage,
-    className: styles.bannerImage
-  };
-
-  const logoutControlAttributes = {
-    className: `application-themed-button ${styles.logoutButton}`,
-    onClick: handleLogoutControlClick
+  const myProfileProperties = {
+    profile: userStorageData
   };
 
   return (
     <div id={styles.homePageMain}>
-      <img {...bannerImageAttributes} alt='home-page-banner' />
-      <h3 className={styles.comingSoonLabel}>Coming soon...</h3>
-      <button {...logoutControlAttributes}>Logout</button>
+      <MyProfile {...myProfileProperties} />
     </div>
   );
 }
