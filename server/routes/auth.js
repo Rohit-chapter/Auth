25e108f080d601f4cc3212f265e85bcf8fbdc6ec
ashayc247 <ashay.t@chapter247.com>
2022-08-01 12:57:31 +0530

    moved server side code to separate repo

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const authController = require('../controllers/auth/auth');
-
-const router = express.Router();
-
-router.get('/getLinkedinProfile', authController.getLinkedinProfile);
-
-module.exports = router;
